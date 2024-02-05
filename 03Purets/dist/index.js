35c413ab3ad9a57bc486539561003378857681d1
martinromario55 <martinromario55@gmail.com>
2024-02-05 10:51:32 +0300

    Public and Private

--- 03Purets/dist/index.js
+++ 03Purets/dist/index.js
@@ -1,11 +1,19 @@
 "use strict";
 // Class
+// class User {
+//   public email: string
+//   name: string
+//   private readonly city: string = 'Kampala'
+//   constructor(email: string, name: string) {
+//     ;(this.email = email), (this.name = name)
+//   }
+// }
 class User {
     constructor(email, name) {
-        this.city = '';
-        ;
-        (this.email = email), (this.name = name);
+        this.email = email;
+        this.name = name;
+        this.city = 'Kampala';
     }
 }
 const htc = new User('htc@h.com', 'htc');
-htc.city = 'Nairobi';
+// htc.city = 'Nairobi'
--- 03Purets/src/index.ts
+++ 03Purets/src/index.ts
@@ -1,12 +1,17 @@
 // Class
+// class User {
+//   public email: string
+//   name: string
+//   private readonly city: string = 'Kampala'
+//   constructor(email: string, name: string) {
+//     ;(this.email = email), (this.name = name)
+//   }
+// }
+
 class User {
-  email: string
-  name: string
-  city: string = ''
-  constructor(email: string, name: string) {
-    ;(this.email = email), (this.name = name)
-  }
+  private readonly city: string = 'Kampala'
+  constructor(public email: string, public name: string) {}
 }
 
 const htc = new User('htc@h.com', 'htc')
-htc.city = 'Nairobi'
+// htc.city = 'Nairobi'
