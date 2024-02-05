ed2b02a7206d9a1cb4d361cd5d4300304f41f8ed
martinromario55 <martinromario55@gmail.com>
2024-02-05 12:17:46 +0300

    Instanceof and Type predicates

--- 03Purets/dist/detection.js
+++ 03Purets/dist/detection.js
@@ -31,3 +31,24 @@ function isAdminAcc(acccount) {
         return acccount.isAdmin;
     }
 }
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return 'fish food';
+    }
+    else {
+        pet;
+        return 'bird food';
+    }
+}
