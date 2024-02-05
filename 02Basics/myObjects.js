2087dc3c321b5c438cb1f3d31e160ecb7f780d37
martinromario55 <martinromario55@gmail.com>
2024-02-05 08:21:02 +0300

    Bad behaviours of objects

---
+++ 02Basics/myObjects.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User = {
+    name: 'hitech',
+    email: 'hitech@lco.dev',
+    isActive: true,
+};
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+createUser({ name: 'Martin', isPaid: false });
---
+++ 02Basics/myObjects.ts
@@ -0,0 +1,23 @@
+const User = {
+  name: 'hitech',
+  email: 'hitech@lco.dev',
+  isActive: true,
+}
+
+function createUser({ name: string, isPaid: boolean }) {}
+
+createUser({ name: 'Martin', isPaid: false })
+
+// Object returning an object
+function createCourse(): { name: string; price: number } {
+  return {
+    name: 'React',
+    price: 100,
+  }
+}
+
+// Pass more information in the function
+let newUser = { name: 'John', isPaid: false, email: 'john@jd.com' }
+createUser(newUser)
+
+export {}
