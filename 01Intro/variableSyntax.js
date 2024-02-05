03fddff4566044ccc84fad9314dd6c5c0b5ad911
martinromario55 <martinromario55@gmail.com>
2024-02-05 07:09:26 +0300

    Number, boolean and type inference

--- 01Intro/variableSyntax.js
+++ 01Intro/variableSyntax.js
@@ -1,2 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Strings
 var greetings = 'Hello Martin';
+greetings.toLowerCase();
 console.log(greetings);
+// Numbers
+var userId = 334455;
+userId = 'hitech';
+// boolean
+var isLoggedIn = false;
--- 01Intro/variableSyntax.ts
+++ 01Intro/variableSyntax.ts
@@ -1,7 +1,15 @@
+// Strings
 let greetings: string = 'Hello Martin'
 
 greetings.toLowerCase()
-
 console.log(greetings)
+
+// Numbers
+let userId = 334455
+
+// userId = 'hitech'
+
+// boolean
+let isLoggedIn: boolean = false
 
 export {}
