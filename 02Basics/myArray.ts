56ff9f30b792f4506124f10ad36e91f4ea739299
martinromario55 <martinromario55@gmail.com>
2024-02-05 09:07:57 +0300

    Arrays

---
+++ 02Basics/myArray.ts
@@ -0,0 +1,25 @@
+const superHeros: string[] = []
+const heroPower: number[] = []
+const heroWeapon: Array<string> = []
+
+type User = {
+  name: string
+  isActive: boolean
+}
+
+const allUsers: User[] = []
+
+// Arrays inside an array
+const MLModels: number[][] = [
+  [1, 2, 3],
+  [4, 5, 6],
+]
+
+superHeros.push('spiderman')
+heroPower.push(100)
+heroWeapon.push('knife')
+
+allUsers.push({
+  name: 'John',
+  isActive: true,
+})
